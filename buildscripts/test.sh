a7e97ee1b5579f8f5e12ace354cc7293ca414f8d
Milan Mertens <m.mertens@metaways.de>
2018-01-05 10:50:21 +0100

    Update test.sh

--- buildscripts/test.sh
+++ buildscripts/test.sh
@@ -1,4 +1,4 @@
-apt-get install graphicsmagick apache2 libapache2-mod-php7.0 composer php7.0-xml php7.0-cli php7.0-mbstring ghostscript unzip git libreoffice
+sudo apt-get install graphicsmagick apache2 libapache2-mod-php7.0 composer php7.0-xml php7.0-cli php7.0-mbstring ghostscript unzip git libreoffice
 
 cp -r test build/
 cp phpunit.xml build
