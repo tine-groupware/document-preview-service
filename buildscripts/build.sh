e0bd0341c5c02acf47627515736159c520894689
mmertens <mmertens@metaways.de>
2018-01-05 10:36:17 +0100

    added buildchain

---
+++ buildscripts/build.sh
@@ -0,0 +1,18 @@
+I: rm -r build
+mkdir build
+
+cp -r config build/
+cp -r public build/
+cp -r src build/
+cp composer.json build/
+cp README.md build/
+
+cd build && mkdir data
+cd build/data && mkdir cache
+
+cd build && composer install
+cd build && composer development-disable
+cd build && rm composer.json
+cd build && rm composer.lock
+
+tar -zcf documentPreview.tar.gz build
---
+++ buildscripts/test.sh
@@ -0,0 +1,5 @@
+apt-get install graphicsmagick apache2 libapache2-mod-php7.0 composer php7.0-xml php7.0-cli php7.0-mbstring ghostscript unzip git libreoffice
+
+cp -r test build/
+cp phpunit.xml build
+cd build && phpunit
