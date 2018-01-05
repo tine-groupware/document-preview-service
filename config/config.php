2f2481e4873b8422512ad11c09158f5eb4521aed
mmertens <mmertens@metaways.de>
2018-01-05 10:34:55 +0100

    added zend exp... infrastruktur

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+use Zend\ConfigAggregator\ArrayProvider;
+use Zend\ConfigAggregator\ConfigAggregator;
+use Zend\ConfigAggregator\PhpFileProvider;
+
+// To enable or disable caching, set the `ConfigAggregator::ENABLE_CACHE` boolean in
+// `config/autoload/local.php`.
+$cacheConfig = [
+    'config_cache_path' => 'data/config-cache.php',
+];
+
+$aggregator = new ConfigAggregator([
+    new ArrayProvider($cacheConfig),
+
+    Auth\ConfigProvider::class,
+
+    DocumentService\ConfigProvider::class,
+
+    new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
+
+    new PhpFileProvider(realpath(__DIR__) . '/development.config.php'),
+], $cacheConfig['config_cache_path']);
+
+return $aggregator->getMergedConfig();
