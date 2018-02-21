e9f48eb4558817dc41653de0d7408d7f4f04ce99
Milan Mertens <mmertens@metaways.de>
2018-02-21 15:35:54 +0100

    deploy script

--- buildscripts/deploy.sh
+++ buildscripts/deploy.sh
@@ -1,1 +1,5 @@
-curl -v --user "$nexususer:$nexuspass" --upload-file ./documentPreview-$CI_COMMIT_REF_NAME.tar.gz https://nexus.metaways.net/repository/documentPreview/$CI_JOB_ID/documentPreview-$CI_COMMIT_REF_NAME.tar.gz
+nexususer=documentPreview
+nexuspass=1cf129712ae98d92f085
+
+curl -v --user "$nexususer:$nexuspass" -H "Content-Type: application/tar+gzip" --upload-file ./documentPreview-$CI_COMMIT_REF_NAME.tar.gz https://nexus.metaways.net/repository/documentPreview/$CI_JOB_ID/documentPreviewService.tar.gz
+curl -v --user "$nexususer:$nexuspass" -H "Content-Type: application/x-debian-package" --upload-file ./documentPreviewService.deb https://nexus.metaways.net/repository/documentPreview/$CI_JOB_ID/documentPreviewService.deb
