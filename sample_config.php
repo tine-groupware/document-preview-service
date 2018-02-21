6886ae63831aca8e542ed34b84710a0074608f4b
Milan Mertens <mmertens@metaways.de>
2018-02-21 09:46:05 +0100

    added / to downurl

--- sample_config.php
+++ sample_config.php
@@ -4,7 +4,7 @@ return [
     'documentService' => [
         "tempDir" => "/tmp", //temp folder
         "downDir" => "/var/www/documentPreviewService/public/download", //download dir for converted files, should be cleaned regularly
-        "downUrl" => "https://127.0.0.1/download", //url for download dir
+        "downUrl" => "https://127.0.0.1/download/", //url for download dir, should end with a /
         "maxProc" => 4, //maximum concurrent conversions
         "loggerOut" => "/var/log/documentPreviewService/doc.log", // log file documentPreview, can be a file or a zend logger
         //list of allowed extensions
