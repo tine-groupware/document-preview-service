23a2ecb9c80bb83b84d869a618ba5e7075685673
Milan Mertens <mmertens@metaways.de>
2018-03-07 12:10:53 +0100

    still fixing sentry

--- src/Factory/SentryIOFactory.php
+++ src/Factory/SentryIOFactory.php
@@ -2,14 +2,14 @@
 namespace DocumentService\Factory;
 
 use Psr\Container\ContainerInterface;
-use DocumentService\Action;
+use DocumentService\Action\SentryIO;
 
 class SentryIOFactory
 {
-    public function __invoke(ContainerInterface $container) : Action\SentryIO
+    public function __invoke(ContainerInterface $container) : SentryIO
     {
         $sentry_url = $container->get('config')['sentryio'];
         if ($sentry_url == null) $sentry_url = '';
-        return new Action\SentryIO($sentry_url);
+        return new SentryIO($sentry_url);
     }
 }
