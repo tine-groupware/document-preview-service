7c6da5a88fc5fac2d4dff680cccc312907c67633
Milan Mertens <mmertens@metaways.de>
2018-03-07 14:59:16 +0100

    changeconfig locations

---
+++ config/autoload/base_config.global.php
@@ -0,0 +1,47 @@
+<?php
+return [
+    // configure for documentPreview
+    'documentService' => [
+        "maxProc" => 4, //maximum concurrent conversions
+        //list of allowed extensions
+        "ext" => [
+            'txt', 'rtf', 'odt', 'ott', 'ods', 'ots', 'odp', 'otp', 'xls', 'xlt', 'xlsx', 'xltx', 'doc', 'dot', 'docx', 'dotx', 'ppt', 'pot', 'pptx', 'potx', 'pdf', 'jpg', 'jpeg', 'gif', 'tiff', 'png'
+        ],
+        //list of libreoffice extensions
+        "docExt" => [
+            'txt', 'rtf', 'odt', 'ott', 'ods', 'ots', 'odp', 'otp', 'xls', 'xlt', 'xlsx', 'xltx', 'doc', 'dot', 'docx', 'dotx', 'ppt', 'pot', 'pptx', 'potx',
+        ],
+        // list of graphicsmagick extensions
+        "imgExt" => [
+            'jpg', 'jpeg', 'gif', 'tiff', 'png'
+        ],
+        // libre office binary
+        "ooBinary"=>'soffice'
+    ],
+    // routing and authentication setup
+    // for authentication documentation see tine20/auth-middleware
+    'routes' => [
+        // list of routes
+        [
+            'name' => 'documentPreviewService', // used for identification
+            'path' => '/v2/documentPreviewService', // uri prefix for route
+            // sequential list of middelware
+            'middleware' => [
+                Auth\Action\NeedsAuth::class, // auth injector
+                Auth\Action\AuthSSL::class, // ssl auth
+                Auth\Action\AuthCheck::class, // auth check
+                DocumentService\Action\DocumentPreview::class, // DocumentPreview middelware
+            ],
+            'allowed_methods' => ['POST'],
+            // auth settings for this route
+            'auth' =>[
+                'required' => true,
+                'permission' => '(1=1)'
+            ]
+        ],
+    ],
+    // auth settings
+    'auth' => [
+        'default' => ['name' => 'default', 'permission' => "false",] // default authentication, if no auth is configured auth will fail
+    ],
+];
