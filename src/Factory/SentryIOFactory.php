045a208e7fa099ddda5dc69274f1386d8fb33620
Milan Mertens <mmertens@metaways.de>
2018-03-07 11:10:31 +0100

    fixed sentry att1

--- src/Factory/SentryIOFactory.php
+++ src/Factory/SentryIOFactory.php
@@ -1,13 +1,12 @@
 <?php
 namespace DocumentService\Factory;
 
-use DocumentService\Action\SentryIO;
 use Psr\Container\ContainerInterface;
 
 class SentryIOFactory
 {
-    public function __invoke(ContainerInterface $container) : SentryIO
+    public function __invoke(ContainerInterface $container) : DocumentService\Action\SentryIO
     {
-        return new SentryIO($container->get('config')['sentryio']);
+        return new DocumentService\Action\SentryIO($container->get('config')['sentryio'], '');
     }
 }
