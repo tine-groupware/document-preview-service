3cddd55db12bfb88110cc272d0ae2b953ad4a0de
Milan Mertens <mmertens@metaways.de>
2018-03-22 11:54:08 +0100

    test pip cp no com.lock

--- buildscripts/regu/test.sh
+++ buildscripts/regu/test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 cp composer.json build/var/www/documentPreviewService/
-cp composer.lock build/var/www/documentPreviewService/
 rm -r build/var/www/documentPreviewService/vendor
 cd build/var/www/documentPreviewService && composer install
 cp -r test build/var/www/documentPreviewService/
