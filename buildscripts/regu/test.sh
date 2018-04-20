f16b3b5e1b0d109ccc02d92836e7cafb310ad333
Milan Mertens <m.mertens@metaways.de>
2018-04-20 09:33:05 +0200

    now uses composer.phar

--- buildscripts/regu/test.sh
+++ buildscripts/regu/test.sh
@@ -2,7 +2,7 @@
 cp composer.json build/var/www/documentPreviewService/
 rm -r build/var/www/documentPreviewService/vendor
 #todo dependecy diff betwean test an deloy/build version
-cd build/var/www/documentPreviewService && composer install
+cd build/var/www/documentPreviewService && php ../../../../composer.phar install
 cp -r test build/var/www/documentPreviewService/
 cp phpunit.xml build/var/www/documentPreviewService/
 cd build/var/www/documentPreviewService && vendor/phpunit/phpunit/phpunit
