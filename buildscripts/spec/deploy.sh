2b355a308fc8eb42982bc6f0f0c09186696eed02
Milan Mertens <m.mertens@metaways.de>
2018-05-17 11:56:43 +0200

    Update deploy.sh

--- buildscripts/spec/deploy.sh
+++ buildscripts/spec/deploy.sh
@@ -3,4 +3,5 @@
 #curl -v --user "documentPreview:1cf129712ae98d92f085" -H "Content-Type: application/x-debian-package" --upload-file ./documentPreviewService-__VERSION_T__.deb https://nexus.metaways.net/repository/documentPreview/__REF_NAME__/__VERSION_PATH__/documentPreviewService.deb
 #curl -v --user "documentPreview:1cf129712ae98d92f085" -H "Content-Type: application/x-debian-package" --upload-file ./documentPreviewService-__VERSION_T__.deb https://nexus.metaways.net/repository/documentPreview/__REF_NAME__/documentPreviewService.deb
 
+ip add
 scp ./documentPreviewService-__VERSION_T__.deb vaptrepo01.lv3.metaways.net:/data/incoming/private/documentservice/xenial/
