52fe9fe6c7dffec6968a30f8f12a19cab008720f
Milan Mertens <m.mertens@metaways.de>
2018-05-24 09:11:07 +0200

    Update deploytorepo.sh user and server

--- buildscripts/spec/deploytorepo.sh
+++ buildscripts/spec/deploytorepo.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-scp ./documentPreviewService-__VERSION_T__.deb vaptrepo01.lv3.metaways.net:/data/incoming/private/documentservice/xenial/
+scp ./documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/xenial/
