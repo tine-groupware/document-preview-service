27ac1afc45704b3b272ebc0d7084ebb6c2038e39
Milan Mertens <m.mertens@metaways.de>
2018-05-24 09:14:00 +0200

    Update deploytorepo.sh remove ./

--- buildscripts/spec/deploytorepo.sh
+++ buildscripts/spec/deploytorepo.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-scp ./documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/xenial/
+scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/xenial/
