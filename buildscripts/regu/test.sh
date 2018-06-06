b87916b6e3ce9c2d926eb0861c7e17e7cd2ef752
Milan Mertens <m.mertens@metaways.de>
2018-06-06 12:01:44 +0200

    usr/share

--- buildscripts/regu/test.sh
+++ buildscripts/regu/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cp composer.json build/var/www/documentPreviewService/
-rm -r build/var/www/documentPreviewService/vendor
+cp composer.json build/usr/share/documentPreviewService/
+rm -r build/usr/share/documentPreviewService/vendor
 #todo dependecy diff betwean test an deloy/build version
-cd build/var/www/documentPreviewService && php ../../../../composer.phar install
-cp -r test build/var/www/documentPreviewService/
-cp phpunit.xml build/var/www/documentPreviewService/
-cd build/var/www/documentPreviewService && vendor/phpunit/phpunit/phpunit
+cd build/usr/share/documentPreviewService && php ../../../../composer.phar install
+cp -r test build/usr/share/documentPreviewService/
+cp phpunit.xml build/usr/share/documentPreviewService/
+cd build/usr/share/documentPreviewService && vendor/phpunit/phpunit/phpunit
