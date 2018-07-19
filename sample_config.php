7fb4fdae92edfd4e608062299232a3b449756e76
Milan Mertens <m.mertens@metaways.de>
2018-07-19 11:12:49 +0200

    Update sample_config.php

--- sample_config.php
+++ sample_config.php
@@ -2,17 +2,19 @@
 return [
     // configure for documentPreview
     'documentService' => [
-        "tempDir" => "/var/lib/documentPreviewService/", //temp folder
+        "tempDir" => "/var/lib/documentPreviewService/", //temp folder, must be rw
         "maxProc" => 4, //maximum concurrent conversions
-        "loggerOut" => "/var/log/documentPreviewService/doc.log", // log file documentPreview, can be a file or a zend logger
+        "loggerOut" => "/var/log/documentPreviewService/doc.log", // log file documentPreview, can be a file path (or a zend logger)
         "ooBinary"=>'soffice'
     ],
-    'authLogger' => '/var/log/documentPreviewService/auth.log', // auth logger, can be a file or a zend logger
+    'authLogger' => '/var/log/documentPreviewService/auth.log', // auth logger, can be a file path (or a zend logger)
     'sentryio' => 'https://<key>:<secret>@sentry.io/<project>', //sentry uri
+    
+    
     'auth' => [
         [
             'name' => 'documentPreviewService', // used for identification
-            'required' => false,
+            'required' => false, // turn auth on or of
             'permission' => '(1=1)'
         ]
     ]
