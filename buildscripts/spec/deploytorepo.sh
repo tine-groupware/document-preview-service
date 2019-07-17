784515f4bc91dc653fc2aa299a5629fdf7b46e1c
Milan Mertens <m.mertens@metaways.de>
2019-07-17 09:06:56 +0200

    feat(ci) deploy version for debian stretch

--- buildscripts/spec/deploytorepo.sh
+++ buildscripts/spec/deploytorepo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/xenial/
 scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/bionic/
-# scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/stretch/
+scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/stretch/
