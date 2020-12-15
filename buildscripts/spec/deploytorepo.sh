f588ee0348003dca9cd7abaea37aa38800eea757
Milan Mertens <m.mertens@metaways.de>
2020-12-15 10:15:25 +0100

    feature(ci): push to focal repo

--- buildscripts/spec/deploytorepo.sh
+++ buildscripts/spec/deploytorepo.sh
@@ -2,3 +2,4 @@
 scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/xenial/
 scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/bionic/
 scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/stretch/
+scp documentPreviewService-__VERSION_T__.deb gitlab@apt.metaways.net:/data/incoming/private/documentservice/focal/
